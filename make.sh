a911add54c0a131f5455aec7ef49f41d693f8e3a
E. G. Bland <egb528@york.ac.uk>
2021-09-09 05:01:41 +0100

    finish json.l

--- make.sh
+++ make.sh
@@ -2,4 +2,6 @@
 
 # takes flex file sans .l as arg, generates and compiles lex.yy.c
 flex $1.l
-gcc -Iinclude -o $1.o lex.yy.c -Llib -lfl -lstack -O3
+# the old gcc with libstack
+#gcc -Iinclude -o $1.o lex.yy.c -Llib -lfl -lstack -O3
+gcc -o $1.o lex.yy.c -lfl -O3
